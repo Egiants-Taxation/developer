ae54e0f3efbd7a9c01a8c6727fa168105be7808d
Ashok <ashoksrm123@gmail.com>
2019-06-13 10:37:18 -0500

    First Commit- "sample"

---
+++ TaxFilingProject/src/test/java/com/egiants/project/taxation/TaxationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.egiants.project.taxation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaxationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
