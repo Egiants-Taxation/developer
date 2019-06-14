b494ed5654fd4133e491bdef04c2e29dd9cfb8c4
Ashok <ashoksrm123@gmail.com>
2019-06-13 21:01:47 -0500

    Updating Maven build error "Temporary Fix"

--- TaxFilingProject/src/main/java/com/egiants/project/taxation/TaxationApplication.java
+++ TaxFilingProject/src/main/java/com/egiants/project/taxation/TaxationApplication.java
@@ -1,12 +1,24 @@
 package com.egiants.project.taxation;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class TaxationApplication {
 
-	public static void main(String[] args) {
+	@RequestMapping("/")
+    String home() {
+        return "Taxation Project Dummy URL to Just Run the Project";
+    }
+	
+	public static void main(String[] args) throws Exception{
 		SpringApplication.run(TaxationApplication.class, args);
 	}
 
